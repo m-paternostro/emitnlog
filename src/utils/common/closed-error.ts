d488e870e3b038afea488efc873af4e0ada7c63e
Marcelo Paternostro <marcelo.paternostro@datadoghq.com>
2025-09-23 16:08:59 -0400

    chore(customErrors): review implementation and add tests

--- src/utils/common/closed-error.ts
+++ src/utils/common/closed-error.ts
@@ -4,7 +4,9 @@
 export class ClosedError extends Error {
   public constructor(message = 'the operation was performed after its scope was closed') {
     super(message);
+    this.name = new.target.name;
+
+    // Not needed for target >= ES2023
     Object.setPrototypeOf(this, new.target.prototype);
-    this.name = 'ClosedError';
   }
 }
---
+++ tests/utils/common/canceled-error.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from '@jest/globals';
+
+import { CanceledError } from '../../../src/utils/index.ts';
+
+describe('emitnlog.utils.canceled-error', () => {
+  test('should pass instanceOf checks', () => {
+    const error = new CanceledError();
+    expect(error).toBeInstanceOf(CanceledError);
+    expect(error).toBeInstanceOf(Error);
+  });
+
+  test('should have the correct name', () => {
+    const error = new CanceledError();
+    expect(error.name).toBe('CanceledError');
+  });
+});
